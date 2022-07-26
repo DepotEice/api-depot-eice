1f6fa488844b98fd964eade7b520584ea1372816
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-26 22:00:44 +0200

    Created and implemented MessageService
    - Service interface
    - Service class
    - Profile
    - Implemented GetByKey(int key) from repository

---
+++ API.DepotEice.BLL/IServices/IMessageService.cs
@@ -0,0 +1,15 @@
+﻿using API.DepotEice.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.DepotEice.BLL.IServices
+{
+    public interface IMessageService
+    {
+        MessageModel? CreateMessage(MessageModel message);
+        IEnumerable<MessageModel> GetUserMessages(string userId);
+    }
+}
---
+++ API.DepotEice.BLL/Profiles/MessageProfile.cs
@@ -0,0 +1,31 @@
+﻿using API.DepotEice.BLL.Models;
+using API.DepotEice.DAL.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.DepotEice.BLL.Profiles
+{
+    public class MessageProfile : Profile
+    {
+        public MessageProfile()
+        {
+            // MessageModel --> MessageEntity
+
+            CreateMap<MessageModel, MessageEntity>()
+                .ForMember(
+                    dest => dest.SenderId,
+                    opt => opt.MapFrom(src => src.Sender.Id))
+                .ForMember(
+                    dest => dest.ReceiverId,
+                    opt => opt.MapFrom(src => src.Receiver.Id));
+
+            // MessageEntity --> MessageModel
+
+            CreateMap<MessageEntity, MessageModel>();
+        }
+    }
+}
