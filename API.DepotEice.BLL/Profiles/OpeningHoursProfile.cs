df52843cc6f4a092f7bb456825b1434faa29b53b
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-29 18:57:17 +0200

    Created and implemented OpeningHoursService
    - Created and implemented service and interface
    - Created Profile

---
+++ API.DepotEice.BLL/Profiles/OpeningHoursProfile.cs
@@ -0,0 +1,25 @@
+﻿using API.DepotEice.BLL.Models;
+using API.DepotEice.DAL.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.DepotEice.BLL.Profiles
+{
+    public class OpeningHoursProfile : Profile
+    {
+        public OpeningHoursProfile()
+        {
+            // OpeningHoursModel --> OpeningHoursEntity
+
+            CreateMap<OpeningHoursModel, OpeningHoursEntity>();
+
+            // OpeningHoursEntity --> OpeningHoursModel
+
+            CreateMap<OpeningHoursEntity, OpeningHoursModel>();
+        }
+    }
+}
