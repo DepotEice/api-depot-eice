6641be47f430739d42a040f6f5fc15fbcf606c5d
Soultan Hatsijev <soultan_98@hotmail.com>
2022-07-31 12:51:16 +0200

    Created User and UserToken service and profile

---
+++ API.DepotEice.BLL/IServices/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using API.DepotEice.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.DepotEice.BLL.IServices
+{
+    public interface IUserService
+    {
+        bool ActivateUser(string id, bool isActive);
+        UserModel? CreateUser(UserModel user);
+        bool DeleteUser(string id);
+        IEnumerable<UserModel> GetUsers();
+        UserModel? GetUser(string id);
+        bool UpdatePassword(string id, string oldPassword, string newPassword, string salt);
+    }
+}
---
+++ API.DepotEice.BLL/Services/UserService.cs
@@ -0,0 +1,131 @@
+﻿using API.DepotEice.BLL.IServices;
+using API.DepotEice.BLL.Models;
+using API.DepotEice.DAL.Entities;
+using API.DepotEice.DAL.IRepositories;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.DepotEice.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ILogger _logger;
+        private readonly IMapper _mapper;
+        private readonly IUserRepository _userRepository;
+        private readonly IRoleRepository _roleRepository;
+
+        public UserService(ILogger<UserService> logger, IMapper mapper,
+            IUserRepository userRepository, IRoleRepository roleRepository)
+        {
+            if (logger is null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
+            if (mapper is null)
+            {
+                throw new ArgumentNullException(nameof(mapper));
+            }
+
+            if (userRepository is null)
+            {
+                throw new ArgumentNullException(nameof(userRepository));
+            }
+
+            if (roleRepository is null)
+            {
+                throw new ArgumentNullException(nameof(roleRepository));
+            }
+
+            _logger = logger;
+            _mapper = mapper;
+            _userRepository = userRepository;
+            _roleRepository = roleRepository;
+        }
+
+        public bool ActivateUser(string id, bool isActive)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            UserEntity? userFromRepo = _userRepository.GetByKey(id);
+
+            if (userFromRepo is null)
+            {
+                _logger.LogWarning(
+                    "{date} - There is no User with ID \"{id}\"!",
+                    DateTime.Now, id);
+
+                return false;
+            }
+
+            return _userRepository.ActivateUser(id, isActive);
+        }
+
+        public UserModel? CreateUser(UserModel user)
+        {
+            if (user is null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            UserEntity userToCreate = _mapper.Map<UserEntity>(user);
+
+            string newId = _userRepository.Create(userToCreate);
+
+            if (string.IsNullOrEmpty(newId))
+            {
+                _logger.LogWarning(
+                    "{date} - The User could not be created!",
+                    DateTime.Now);
+
+                return null;
+            }
+
+            UserEntity? userFromRepo = _userRepository.GetByKey(newId);
+
+            if (userFromRepo is null)
+            {
+                _logger.LogError(
+                    "{date} - The retrieval of the newly created User with ID \"{id}\" " +
+                    "returned null!",
+                    DateTime.Now, newId);
+
+                return null;
+            }
+
+            UserModel userModel = _mapper.Map<UserModel>(userFromRepo);
+
+
+
+            return userModel;
+        }
+
+        public bool DeleteUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserModel? GetUser(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdatePassword(string id, string oldPassword, string newPassword, string salt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
