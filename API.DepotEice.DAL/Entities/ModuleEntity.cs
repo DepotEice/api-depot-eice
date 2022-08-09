6f581fcc7b44bf9ecee1284134cd1b4d327013fb
Soultan Hatsijev <soultan_98@hotmail.com>
2022-08-09 20:17:23 +0200

    Removed all constructors

--- API.DepotEice.DAL/Entities/ModuleEntity.cs
+++ API.DepotEice.DAL/Entities/ModuleEntity.cs
@@ -25,59 +25,5 @@ namespace API.DepotEice.DAL.Entities
         /// <see cref="ModuleEntity"/>'s description column in the database
         /// </summary>
         public string Description { get; set; }
-
-        /// <summary>
-        /// Instanciate an object <see cref="ModuleEntity"/> with all its properties
-        /// </summary>
-        /// <param name="id">Module's ID</param>
-        /// <param name="name">Module's name</param>
-        /// <param name="description">Module's description</param>
-        /// <exception cref="ArgumentOutOfRangeException"></exception>
-        /// <exception cref="ArgumentNullException"></exception>
-        public ModuleEntity(int id, string name, string description)
-        {
-            if (id < 0)
-            {
-                throw new ArgumentOutOfRangeException(nameof(id));
-            }
-
-            if (string.IsNullOrEmpty(name))
-            {
-                throw new ArgumentNullException(nameof(name));
-            }
-
-            if (string.IsNullOrEmpty(description))
-            {
-                throw new ArgumentNullException(nameof(description));
-            }
-
-            Id = id;
-            Name = name;
-            Description = description;
-        }
-
-        /// <summary>
-        /// Instanciate an object <see cref="ModuleEntity"/> with all its properties except
-        /// <see cref="Id"/> that is 0
-        /// </summary>
-        /// <param name="name">Module's name</param>
-        /// <param name="description">Module's description</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public ModuleEntity(string name, string description)
-        {
-            if (string.IsNullOrEmpty(name))
-            {
-                throw new ArgumentNullException(nameof(name));
-            }
-
-            if (string.IsNullOrEmpty(description))
-            {
-                throw new ArgumentNullException(nameof(description));
-            }
-
-            Id = 0;
-            Name = name;
-            Description = description;
-        }
     }
 }
