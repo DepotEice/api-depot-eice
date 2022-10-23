32da9630f395f425c3e44d7030a793c5ea020ef8
Soultan Hatsijev <soultan_98@hotmail.com>
2022-10-23 16:29:54 +0200

    deleted BLL folder

--- API.DepotEice.BLL/IServices/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿using API.DepotEice.BLL.Dtos;
-
-namespace API.DepotEice.BLL.IServices;
-
-public interface IAuthService
-{
-    UserDto? SignIn(string email, string password, string salt);
-    bool SingUp(UserDto dto, string salt);
-}
